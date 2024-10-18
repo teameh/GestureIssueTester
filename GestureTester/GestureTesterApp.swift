22f4582c4baa6ddc4446b44c64d0e5dff0254c5f
Teameh <1330668+teameh@users.noreply.github.com>
2024-10-18 16:21:39 +0200

    Initial Commit

---
+++ GestureTester/GestureTesterApp.swift
@@ -0,0 +1,17 @@
+//
+//  GestureTesterApp.swift
+//  GestureTester
+//
+//  Created by Tieme on 18/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct GestureTesterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
