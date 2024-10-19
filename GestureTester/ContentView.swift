7f546d6474385d4f2083c5b12edf9b41574f8d9a
Teameh <1330668+teameh@users.noreply.github.com>
2024-10-19 15:05:22 +0200

    Updated code to DTS-engineer example

--- GestureTester/ContentView.swift
+++ GestureTester/ContentView.swift
@@ -1,88 +1,55 @@
-//
-//  ContentView.swift
-//  GestureTester
-//
-//  Created by Tieme on 18/10/2024.
-//
-
 import SwiftUI
 
+struct Model: Equatable {
+    var rotation: Angle = .zero
+    var magnification: CGFloat = 1.0
+}
+
 struct ContentView: View {
-    struct ManipulationState: Equatable {
-        var magnification: CGFloat?
-        var rotation: Angle?
-    }
+    @GestureState private var gestureState = Model()
 
-    @State var rectangleWidth: CGFloat = 200.0
-    @State var rectangleHeight: CGFloat = 200.0
-    @State var rectangleRotation: CGFloat = 0
-
-    @GestureState var state: ManipulationState? = nil
-
-    var body: some View {
-        Rectangle()
-            .fill(.orange)
-            .shadow(radius: 10)
-            .frame(
-                width: rectangleWidth * (state?.magnification ?? 1),
-                height: rectangleHeight * (state?.magnification ?? 1)
-            )
-            .rotationEffect(.degrees(rectangleRotation + (state?.rotation?.degrees ?? 0)))
-            .gesture(gesture)
-            .onChange(of: state, { oldValue, newValue in
-                if oldValue == nil, let newValue {
-                    print("Gesture started nil -> (\(newValue))")
-                }
-
-                if let oldValue, let newValue {
-                    print("Gesture updated (\(oldValue)) -> (\(newValue))")
-                }
-
-                if let oldValue, newValue == nil {
-                    print("Gesture ended, (\(oldValue)) -> nil")
-                    gestureDidEnd(state: oldValue)
-                }
-            })
-    }
-
-    var gesture: GestureStateGesture<SimultaneousGesture<RotateGesture, MagnifyGesture>, ManipulationState?> {
+    var simultaneousGesture: some Gesture {
         SimultaneousGesture(
             RotateGesture(minimumAngleDelta: .degrees(5)),
             MagnifyGesture(minimumScaleDelta: 0.1)
         )
-            .updating($state) { value, state, transaction in
-                var rotation = value.first?.rotation
-                if let radians = rotation?.radians, radians.isNormal != true {
-                    print("Rotation is not normal? \(radians)")
-                    rotation = nil
-                }
-
-                state = ManipulationState(
-                    magnification: value.second?.magnification,
-                    rotation: rotation
-                )
+        .updating($gestureState) { value, state, _ in
+            if let rotation = value.first {
+                state.rotation = rotation.rotation
             }
+            if let magnification = value.second {
+                state.magnification = magnification.magnification
+            }
+        }
+        .onChanged { value in
+            if let rotation = value.first?.rotation {
+                print("Rotation onChange: \(rotation.degrees) degrees")
+            }
+            if let magnification = value.second?.magnification {
+                print("Magnification onChange: \(magnification)")
+            }
+
+        }
+        .onEnded { value in
+            if let rotation = value.first?.rotation {
+                print("Rotation ended at: \(rotation.degrees) degrees")
+            }
+            if let magnification = value.second?.magnification {
+                print("Magnification ended at: \(magnification)")
+            }
+        }
     }
 
-    func gestureDidEnd(state: ManipulationState) {
-        // Snap rectangle to 15 degrees
-        if let rotation = state.rotation {
-            let totalRotation = rectangleRotation + rotation.degrees
-            rectangleRotation = totalRotation - totalRotation.remainder(dividingBy: 15)
-        }
-
-        if let magnification = state.magnification {
-            rectangleWidth = rectangleWidth * magnification
-            rectangleHeight = rectangleHeight * magnification
-        }
-    }
-}
-
-extension ContentView.ManipulationState: CustomStringConvertible {
-    var description: String {
-        [
-            magnification.map { String(format: "magnify: %0.2f", $0) },
-            rotation.map { String(format: "rotate: %0.2f", $0.degrees) }
-        ].compactMap { $0 }.joined(separator: ", ")
+    var body: some View {
+        Rectangle()
+            .fill(Color.orange)
+            .frame(width: 200, height: 200)
+            .shadow(radius: 10)
+            .scaleEffect(gestureState.magnification)
+            .rotationEffect(gestureState.rotation)
+            .gesture(simultaneousGesture)
+            .onChange(of: gestureState) {
+                print(gestureState)
+            }
     }
 }
